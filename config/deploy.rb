23be91b09421a8c4c473260f4501722b1f7a0665
matiasmenares <dejaaymts@gmail.com>
2016-10-08 19:30:14 -0300

    capistrano config

---
+++ config/deploy.rb
@@ -0,0 +1,68 @@
+# config valid only for current version of Capistrano
+directory = "apptec"
+set :application, 'sii-api'
+set :repo_url, ENV['MY_GIT_URL'].to_s+"Sii-api.git"
+set :scm, :git
+set :rails_env, "production"
+set :branch, "master"
+set :user, "apptec"
+set :deploy_to, "/home/"+directory+"/rails_app/api_sii"
+set :group, "deployers"
+set :rails_env, "production"
+set :deploy_via, :copy
+set :pty, true
+#set :default_env, { path: "/usr/local/rvm/rubies/ruby-2.2.3/bin:$PATH" }
+set :tmp_dir, "/home/"+directory+"/tmp"
+set :assets_roles, [:web, :app]
+server "209.133.207.11", roles: [:app, :web, :db], :primary => true
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+  desc "Restart application"
+  task :restart do
+	on roles(:web) do
+    	execute "touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+    end
+  end
+  desc "Data DB"
+  task :seed do
+    on primary :db do
+      within current_path do
+        with rails_env: fetch(:stage) do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+end
+after "deploy", "deploy:restart"
