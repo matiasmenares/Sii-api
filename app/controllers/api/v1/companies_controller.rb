e72a51e095ac5f9a4014b14f224ba862e1b9ce02
matiasmenares <dejaaymts@gmail.com>
2017-04-15 14:03:40 -0300

    Response False

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -4,14 +4,14 @@ class Api::V1::CompaniesController < ApplicationController
 		if validate(params[:rut])
 			search = Company.where("rut LIKE ?","%"+params[:rut]+"%").first
 			if !search.blank?
-				response = {response:"true",data: search}
+				response = {response: true ,data: search }
 				render json: response, status: 200
 			else
-				response = {response:"false",msg:"Rut no existe en la base de datos"}
+				response = {response: false ,msg: "Rut no existe en la base de datos"}
 				render json: response,status: 422
 			end
 		else
-			response = {response:"false",msg:"Rut no valido"}
+			response = {response: false ,msg: "Rut no valido"}
 			render json:response,status: 422
 		end
 	end
